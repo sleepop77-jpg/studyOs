1d6065fba25f47d82cc8388941b42ecf75593df8
sleepop77-jpg <sleepop77@gmail.com>
2026-08-25 20:07:10 +0530

    Bridge: edit app/src/main/java/com/example/studyos/ui/lockdown/LockdownScreen.kt

--- app/src/main/java/com/example/studyos/ui/lockdown/LockdownScreen.kt
+++ app/src/main/java/com/example/studyos/ui/lockdown/LockdownScreen.kt
@@ -6,6 +6,7 @@ import android.provider.Settings
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
